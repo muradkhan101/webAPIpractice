0f106c04fd38692a59065b6210433072e3099fd9
Murad Khan <muradkhan101@gmail.com>
2018-05-24 20:51:35 -0700

    Updated code for automata

--- src/rotations/automata.js
+++ src/rotations/automata.js
@@ -1,51 +1,41 @@
 function drawStep(prev, current, row, col, damp) {
-    current[row][col].x = (
-        prev[row - 1][col].x +
-        prev[row + 1][col].x +
-        prev[row][col - 1].x + 
-        prev[row][col + 1].x
-    ) / 2 - current[row][col].x;
-
-    current[row][col].x = current[row][col].x * damp;
-
-    current[row][col].y = (
-        prev[row - 1][col].y +
-        prev[row + 1][col].y +
-        prev[row][col - 1].y + 
-        prev[row][col + 1].y
-    ) / 2 - current[row][col].y;
-
-    current[row][col].y = current[row][col].y * damp;
-}
-function draw(rows, cols, damp) {
-    for (let i = 1; i < rows - 1; i++) {
-        for (let j = 1; j < cols - 1; j++) {
-            drawStep(prev, current, i, j, damp);
-        }
-    }
+    let item = current[row][col];
     
-    updateSquares(document.querySelectorAll('.square'), current, cols);
+    Object.keys(item).forEach(key => {
+        item[key] = (
+            prev[row - 1][col][key] +
+            prev[row + 1][col][key] +
+            prev[row][col - 1][key] + 
+            prev[row][col + 1][key]
+        ) / 2 - current[row][col][key];
+        item[key] = item[key] * damp;
+    })
 
-    let temp = prev;
-    prev = current;
-    current = temp;
-    window.requestAnimationFrame(() => draw(rows, cols, damp));
+}
+function draw(rows, cols, damp, transformer) {
+    let shouldDraw = true;
+    function inner() {
+         for (let i = 1; i < rows - 1; i++) {
+            for (let j = 1; j < cols - 1; j++) {
+                drawStep(prev, current, i, j, damp);
+            }
+        }
+        updateSquares(document.querySelectorAll('.square'), transformer, current, cols);
+
+        let temp = prev;
+        prev = current;
+        current = temp;
+        if (shouldDraw)
+            window.requestAnimationFrame(() => inner());
+    }
+   inner();
+    return () => (shouldDraw = !shouldDraw, inner());
 }
 
-function updateSquares(squares, current, cols) {
+function updateSquares(squares, transformer, current, cols) {
     current.forEach((row, i) => {
         row.forEach((item, j) => {
-            setTransform(squares[j + i * cols], current[i][j])
+            transformer(squares[j + i * cols], current[i][j])
         })
-    })
-}
-
-function setUpListener() {
-    document.querySelector('.square-grid').addEventListener('click', (e) => {
-        let target = e.target;
-        let row = +target.getAttribute('row');
-        let col = +target.getAttribute('column');
-        current[row][col].x = 255;
-        current[row][col].y = 255;
     })
 }
--- src/rotations/rotations.js
+++ src/rotations/rotations.js
@@ -36,71 +36,9 @@ function setTransform(square, amount) {
     let transform = `rotateX(${getDegX(amount)}deg) rotateY(${getDegY(amount)}deg)`;
     square.style.transform = transform;
 }
-
-let NUM_ROWS = 24;
-let NUM_COLS = 24;
-let SQ_SIZE = 25;
-
 let current = Array.from({length: NUM_ROWS}, () => Array.from({length: NUM_COLS}, () => ({x:128, y:128}) ));
 let prev = Array.from({length: NUM_ROWS}, () => Array.from({length: NUM_COLS}, () => ({x:128, y:128}) ));;
 
-let makeSquare = (row, col) => {
-    let el = document.createElement('div');
-    el.classList.add('square');
-    el.setAttribute('row', row);
-    el.setAttribute('column', col);
-    return el;
-}
-
-let makeGrid = (rows, cols) => {
-    let grid = document.createElement('div');
-    grid.classList.add('square-grid');
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            grid.appendChild(makeSquare(i, j));
-        }
-    }
-    return grid;
-}
-
-let makeEverything = (appendTo = document.querySelector('body'), rows = NUM_ROWS, cols = NUM_COLS, sq_size = SQ_SIZE) => {
-    let styles = document.createElement('style');
-    styles.type = 'text/css';
-    styles.innerHTML = `
-        .square {
-            width: ${sq_size}px;
-            height: ${sq_size}px;
-            background: cadetblue;
-        }
-        .square-grid {
-            display: flex;
-            flex-wrap: wrap;
-            width: ${sq_size * cols}px;
-            height: ${sq_size * rows}px;
-        }
-    `;
-    document.querySelector('head').appendChild(styles);
-    let grid = makeGrid(rows, cols);
-    appendTo.appendChild(grid);
-}
-
-function debounceEvent(container, event, fn, timer = 1000 / 15) {
-    let shouldFire = true;
-    let timerId = null;
-    function eventWrapper(e) {
-        if (shouldFire) {
-            fn(container, e);
-            shouldFire = false;
-            timerId = null;
-        } else if (!timerId) {
-            timerId = setTimeout( () => shouldFire = true, timer);
-        }
-    }
-    container.addEventListener(event, eventWrapper);
-    return function removeListener() {
-        container.removeEventListener(event, eventWrapper);
-    }
-}
 
 // debounceEvent(document, 'mousemove', (container, e) => {
 //     let page = getMousePos(e);
